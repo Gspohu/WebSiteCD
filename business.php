0691851ed07e335c29cd975fe07c14992ef04eb6
Gspohu <pohuvalentin@gmail.com>
2017-01-26 10:51:13 +0100

    Change name of page pro to business

--- business.php
+++ business.php
@@ -8,13 +8,13 @@ include_once('model/connexion_sql.php');
 <head>
 	<meta charset="utf-8" />
 	<title>Cairn Devices – Upgrade in freedom</title>
-	<link href="css/pro" rel="stylesheet" />
+	<link href="css/business" rel="stylesheet" />
 	<script type="text/javascript" src="js/piwik/piwik.js"></script>
 </head>
 
 <div class="conteneur" >
 	<?php
-	include_once('controller/pro.php');
+	include_once('controller/business.php');
 	?>
 </div>
 
