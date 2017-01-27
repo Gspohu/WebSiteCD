48b8bff91f5c428b2da86fd2a863ce59e759c51b
gspohu <pohuvalentin@gmail.com>
2017-01-27 14:52:15 +0100

    Fix bug on su dev

--- WebsiteCD-install.sh
+++ WebsiteCD-install.sh
@@ -1280,7 +1280,7 @@ Install_Serge()
 	# Download Serge
 	wget https://github.com/ABHC/SERGE/archive/web-interface.zip # HACK je suis sur la branche webUI attention
 	mkdir /var/www/Serge/
-	unzip master.zip -d /var/www/Serge/
+	unzip  -d web-interface.zip /var/www/Serge/
 	rsync -a /var/www/Serge/SERGE-web-interface/ /var/www/Serge/
 	chmod -R 777 /var/www/Serge
 	rm web-interface.zip
@@ -2178,13 +2178,13 @@ Dev_utils()
 	# Ask for git name
 	while [ "$gitname" == "" ]
 	do
-		dialog --backtitle "Cairngit installation" --title "Email for git"\
+		dialog --backtitle "Cairngit installation" --title "Name for git"\
 		--inputbox "    /!\\ This name will be use in your commits /!\\" 7 60 2> $FICHTMP
 		gitname=`cat $FICHTMP`
 	done
-	su dev -c "git config --global user.email '$gitemail'"
-	su dev -c "git config --global user.name '$gitname'"
-	su dev -c "git config --global push.default simple"
+	echo "$adminPass" | su dev -c "git config --global user.email '$gitemail'"
+	echo "$adminPass" | su dev -c "git config --global user.name '$gitname'"
+	echo "$adminPass" | su dev -c "git config --global push.default simple"
 
 	cd ~ || { echo "FATAL ERROR : cd command fail to go to ~"; exit 1; }
 
